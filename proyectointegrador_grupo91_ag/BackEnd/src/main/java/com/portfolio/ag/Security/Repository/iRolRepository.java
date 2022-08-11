055eead909395455095cc7a1dd5cccfd51380733
ariel1433 <arielgastaminza@gmail.com>
2022-08-11 16:28:26 -0300

    BACKEND
    Continuo modificando backend
    Implementacion de JWT

---
+++ proyectointegrador_grupo91_ag/BackEnd/src/main/java/com/portfolio/ag/Security/Repository/iRolRepository.java
@@ -0,0 +1,13 @@
+package com.portfolio.ag.Security.Repository;
+
+import com.portfolio.ag.Security.Entity.Rol;
+import com.portfolio.ag.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
