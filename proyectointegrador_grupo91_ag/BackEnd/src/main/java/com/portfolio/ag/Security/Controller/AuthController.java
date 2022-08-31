f8ccc389cc7193ea845e8ff9e112c74dc0eef97b
ariel1433 <arielgastaminza@gmail.com>
2022-08-31 17:08:07 -0300

    DEPLOY
    DEPLOY
    CORS
    HEROKU
    CLODCLEVER
    FIREBASE

--- proyectointegrador_grupo91_ag/BackEnd/src/main/java/com/portfolio/ag/Security/Controller/AuthController.java
+++ proyectointegrador_grupo91_ag/BackEnd/src/main/java/com/portfolio/ag/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://front-end-angular-ag.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
